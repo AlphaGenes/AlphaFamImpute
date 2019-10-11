324f8a8e8f0f110b9bd3164afd9fa040f387f408
WHALEN Andrew <awhalen@mvm-ri-m155130.roslin.ed.ac.uk>
2019-10-11 14:46:46 +0100

    Updating build file and re-testing

--- build.sh
+++
@@ -1,8 +0,0 @@
-python setup.py bdist_wheel
-
-rm -rf AlphaFamImpute_dist
-mkdir AlphaFamImpute_dist
-cp dist/* AlphaFamImpute_dist
-cp -r example AlphaFamImpute_dist
-zip -r AlphaFamImpute.zip AlphaFamImpute_dist
-
--- build_pipeline.sh
+++ build_pipeline.sh
@@ -1,13 +1,18 @@
+
+# Create python wheel.
 rm -r build
 rm -r dist
 python setup.py bdist_wheel
-pip uninstall AlphaFamImpute -y
-pip install dist/AlphaFamImpute-0.1-py3-none-any.whl
+
+#Compile manual
+ ( cd docs; make latexpdf )
 
 
-rm -rf AlphaFamImpute_dist
-mkdir AlphaFamImpute_dist
-cp dist/* AlphaFamImpute_dist
-cp -r example AlphaFamImpute_dist
-zip -r AlphaFamImpute.zip AlphaFamImpute_dist
+target=AlphaFamImpute
+rm -rf $target
+mkdir $target
+cp dist/* $target
+cp docs/build/latex/AlphaFamImpute.pdf $target
+cp -r example/* $target
+zip -r $target.zip AlphaFamImpute
 
